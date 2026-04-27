44d3a39ef97a310eddfe24fea94971bfc984d02f
MISTICMAN23 <eksperementy300@gmail.com>
2026-04-27 23:16:29 +0300

    I tried to make normal texture loading, something is wrong with it ;3

---
+++ src/utils/texture/texture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by misticman23 on 4/27/26.
+//
+
+#include "texture.h"
---
+++ src/utils/texture/texture.h
@@ -0,0 +1,13 @@
+//
+// Created by misticman23 on 4/27/26.
+//
+
+#ifndef ENGINE_TEXTURE_H
+#define ENGINE_TEXTURE_H
+
+
+class texture {
+};
+
+
+#endif //ENGINE_TEXTURE_H
