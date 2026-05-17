b56e66d550456c84034496d11f82be30ffa30ec5
MISTICMAN23 <eksperementy300@gmail.com>
2026-05-17 12:51:51 +0300

    First commit(HAVE ERROR!)

---
+++ src/spectrumui/spectrumui.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Ultralight/Ultralight.h>
+#include <Ultralight/Renderer.h>
+
+class SpectrumUI {
+    public:
+        void UI();
+        void RenderUI();
+        void ReloadUI();
+    private:
+        //auto& platform = Platform::instance();
+};
